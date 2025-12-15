f8638a25194626d8f7e3880f17ca45fec8a1094c
MK Rabbani <133435149+mkError420@users.noreply.github.com>
2025-12-16 01:53:04 +0600

    Admin page update

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Image from "next/image";
+import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
 
 const FormModal = ({table,type,data,id}:{
   table:"teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement";
@@ -11,11 +13,39 @@ const FormModal = ({table,type,data,id}:{
 
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7"
   const bgColor = type === "create" ? "bg-cyan-400" : type=== 'update' ? "bg-sky-400" : "bg-purple-400";
+
+    const [open, setOpen] = useState(false);
+
+
+
+    const Form = ()=>{
+      return type === "delete" && id ? ( 
+      <form action="" className="p-4 flex flex-col gap-4 ">
+        <span className="text-center font-medium">You will loss this data 😦 !! Do you want to delete this {table}?</span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center ">Delete</button>
+      </form>) :
+      (
+        <TeacherForm type="create"/>
+      );
+    }
+  
+
   return (
      <>
-  <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`}>
+  <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+  onClick={()=>setOpen(true)}
+  >
     <Image src={`/${type}.png`} alt="" width={16} height={16}/>
     </button>
+    {open && <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+      <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%]2xl:w-[40%]">
+        <Form/>
+        <div className="absolute top-4 right-4 curs cursor-pointer " onClick={()=>setOpen(false)}>
+        <Image src="/close.png" alt="" width={14} height={14}/>
+        </div>
+      </div>
+      
+    </div>}
     </>
   );
 };
